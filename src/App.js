d5262495d40fba7e812754db100a234dd745a4b1
Cesar Martinez <cmarvarc@hotmail.com>
2023-08-23 18:13:33 -0500

    Changes made to render components into App page. Will correct later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Button } from './Components/Button/Button';
+import Input from './Components/Input/input';
 import './App.css';
+
 
 function App() {
   return (
     <div>
       <div className='container'>
-        <h1 className='title debug'>Gym Buddies</h1>
+        <h1 className='title'>Gym Buddies</h1>
+        <Input 
+          label="Email"
+          placeholder="example@mail.com"
+          value="test"
+        />
+        <Button>Login</Button>
       </div>
     </div>
   );
