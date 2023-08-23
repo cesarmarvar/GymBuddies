201f6c9518722a60ec6dcfcf6ed38be58ddaec9f
Cesar Martinez <cmarvarc@hotmail.com>
2023-08-23 18:12:35 -0500

    Inserted global css to Index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Global } from '@emotion/react';
+import { reset, global } from './Styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
+    <Global styles={global} />
     <App />
   </React.StrictMode>
 );
